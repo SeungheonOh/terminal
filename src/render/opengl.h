88295ef502c3c919fa8f066b55afc20d93146a35
SeungheonOh <dan.oh0721@gmail.com>
2021-03-30 20:28:28 -0500

    apple

--- src/render/opengl.h
+++ src/render/opengl.h
@@ -1,2 +1,9 @@
-#include <GL/glew.h>
-#include <GL/gl.h>
+#if defined(__APPLE__)
+# pragma clang diagnostic ignored "-Wdeprecated-declarations"
+//# include <OpenGL/gl.h>
+# define GLEW_STATIC
+# include <GL/glew.h>
+#elif
+# include <GL/glew.h>
+# include <GL/gl.h>
+#endif
