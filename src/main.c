2da9a2b50b9f04b262bec6628937ad63ab2049a0
SeungheonOh <dan.oh0721@gmail.com>
2021-04-09 22:42:49 -0500

    proper macos

--- src/main.c
+++ src/main.c
@@ -22,8 +22,10 @@ int main() {
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  // glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-  // glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#if defined(__APPLE__)
+  glfwWindowHint (GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint (GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#endif
 
   GLFWwindow *window = glfwCreateWindow(500, 500, "Terminal", NULL, NULL);
   if (window == NULL) {
